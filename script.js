b619bcd85224394c37a6545d88eef44b3826f4e9
KRISHNA SHARMA <123051328+krishnasharma8@users.noreply.github.com>
2024-08-28 14:50:57 +0530

    Add files via upload
    MongoDB,Mongoose(Current)

---
+++ script.js
@@ -0,0 +1,16 @@
+// BSON >> JSON
+// cli command line interface 
+const data=[
+{name:"krishna",age:20,marks:90,subject:Backend},
+{name:"mohit",age:21,marks:80,subject:Electronic},
+{name:"Arushi",age:30,marks:89,subject:Business},
+{name:"Divyam",age:28,marks:90,subject:Marketing},
+{name:"Deepak",age:38,marks:95,subject:Accounts}
+]
+[
+    { "name": "Kabir Das", "age": 22, "subject": "c++", "rating": 4.5 },
+    { "name": "Hari Das", "age": 24, "subject": "java", "rating": 4.8 },
+    { "name": "Ameer Chauhan", "age": 20, "subject": "DBMS", "rating": 4.2 },
+    { "name": "Anuj Chaughary", "age": 23, "subject": "BEE", "rating": 3.9 },
+    { "name": "Gurhind Singh", "age": 21, "subject": "Python", "rating": 4.7 }
+]
