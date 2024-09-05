c1892d0b4297b4227b431a0e4be551f407990b63
KRISHNA SHARMA <123051328+krishnasharma8@users.noreply.github.com>
2024-09-05 11:26:55 +0530

    Install react js and tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+// index.js or App.js
+// index.js or App.js
+import './index.css'; 
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500">app</h1>
+    </div>
+  );
+}
